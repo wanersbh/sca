f403d89f66532d967c0ee920ed47d4f601e70b12
waner <wanersbh@gmail.com>
2019-07-29 11:13:07 -0300

    Criação do projeto de ativos.

---
+++ sca-ativos/src/main/java/com/sca/ativo/ScaAtivosApplication.java
@@ -0,0 +1,13 @@
+package com.sca.ativo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScaAtivosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScaAtivosApplication.class, args);
+	}
+
+}
