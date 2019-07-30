b454f5dedd04fc2f2e27d5b33924bd15c0afc051
waner.silva <waner.silva@tjmg.jus.br>
2019-07-30 18:48:55 -0300

    Criação do método para criar categoria e para recuperar através do cod.

--- sca-ativos/src/main/java/com/sca/ativo/controller/CategoriaController.java
+++ sca-ativos/src/main/java/com/sca/ativo/controller/CategoriaController.java
@@ -1,11 +1,17 @@
 package com.sca.ativo.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.sca.ativo.model.Categoria;
 import com.sca.ativo.repository.CategoriaRepository;
@@ -16,10 +22,25 @@ public class CategoriaController {
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
-	
+
 	@GetMapping
-	public List<Categoria> listar(){
+	public List<Categoria> listar() {
 		return categoriaRepository.findAll();
 	}
-	
+
+	@PostMapping
+	public ResponseEntity<Categoria> criar(@RequestBody Categoria categoria) {
+		Categoria categoriaSalva = categoriaRepository.save(categoria);
+
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
+				.buildAndExpand(categoriaSalva.getCodigo()).toUri();
+
+		return ResponseEntity.created(uri).body(categoriaSalva);
+	}
+
+	@GetMapping("/{codigo}")
+	public Categoria buscaPorCodigo(@PathVariable Long codigo) {
+		return categoriaRepository.findById(codigo).orElse(null);
+	}
+
 }
