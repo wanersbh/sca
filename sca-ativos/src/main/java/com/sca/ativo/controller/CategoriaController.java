3d7e587cb54158c4f26fb975d1f23bc868f51025
waner <wanersbh@gmail.com>
2019-07-31 10:25:25 -0300

    Ajustes no cadastro de categorias.

--- sca-ativos/src/main/java/com/sca/ativo/controller/CategoriaController.java
+++ sca-ativos/src/main/java/com/sca/ativo/controller/CategoriaController.java
@@ -1,9 +1,13 @@
 package com.sca.ativo.controller;
 
-import java.net.URI;
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,8 +15,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.sca.ativo.event.RecursoCriadoEvent;
 import com.sca.ativo.model.Categoria;
 import com.sca.ativo.repository.CategoriaRepository;
 
@@ -23,24 +27,29 @@ public class CategoriaController {
 	@Autowired
 	private CategoriaRepository categoriaRepository;
 
+	@Autowired
+	private ApplicationEventPublisher publisher;
+	
+	
 	@GetMapping
 	public List<Categoria> listar() {
 		return categoriaRepository.findAll();
 	}
 
 	@PostMapping
-	public ResponseEntity<Categoria> criar(@RequestBody Categoria categoria) {
+	public ResponseEntity<Categoria> criar(@Valid @RequestBody Categoria categoria, HttpServletResponse response) {
 		Categoria categoriaSalva = categoriaRepository.save(categoria);
+		publisher.publishEvent(new RecursoCriadoEvent(this, response, categoriaSalva.getCodigo()));
 
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
-				.buildAndExpand(categoriaSalva.getCodigo()).toUri();
-
-		return ResponseEntity.created(uri).body(categoriaSalva);
+		return ResponseEntity.status(HttpStatus.CREATED).body(categoriaSalva);
 	}
 
 	@GetMapping("/{codigo}")
-	public Categoria buscaPorCodigo(@PathVariable Long codigo) {
-		return categoriaRepository.findById(codigo).orElse(null);
+	public ResponseEntity<Categoria> buscaPorCodigo(@PathVariable Long codigo) {
+		Categoria categoriaRetornada = categoriaRepository.findById(codigo).orElse(null);
+
+		return categoriaRetornada != null ? ResponseEntity.ok(categoriaRetornada) : 
+			ResponseEntity.notFound().build();
 	}
 
 }
---
+++ sca-ativos/src/main/java/com/sca/ativo/event/listener/RecursoCriadoListener.java
@@ -0,0 +1,32 @@
+package com.sca.ativo.event.listener;
+
+import java.net.URI;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.sca.ativo.event.RecursoCriadoEvent;
+
+@Component
+public class RecursoCriadoListener implements ApplicationListener<RecursoCriadoEvent>{
+
+	@Override
+	public void onApplicationEvent(RecursoCriadoEvent recursoCriadoEvent) {
+		HttpServletResponse response = recursoCriadoEvent.getResponse();
+		Long codigo = recursoCriadoEvent.getCodigo();
+		
+		adicionarHeaderLocation(response, codigo);
+	}
+
+	private void adicionarHeaderLocation(HttpServletResponse response, Long codigo) {
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
+				.buildAndExpand(codigo).toUri();
+		
+		response.setHeader("Location", uri.toASCIIString());
+	}
+
+}
