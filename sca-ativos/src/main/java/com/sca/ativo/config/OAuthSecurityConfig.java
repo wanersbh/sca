7ea9151d8e8afee8c745da7c987f8a21af9a1ca4
waner.silva <waner.silva@tjmg.jus.br>
2019-08-05 07:55:25 -0300

    Ajuste na segurança da api.

--- sca-ativos/src/main/java/com/sca/ativo/config/OAuthSecurityConfig.java
+++ sca-ativos/src/main/java/com/sca/ativo/config/OAuthSecurityConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
@@ -20,7 +20,7 @@ public class OAuthSecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Bean
 	public PasswordEncoder passwordEncoder() {
-		return NoOpPasswordEncoder.getInstance();
+	    return new BCryptPasswordEncoder();
 	}
 
 }
