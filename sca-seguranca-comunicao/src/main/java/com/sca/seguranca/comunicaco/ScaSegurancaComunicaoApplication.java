7929e1110de5fdc3339f9bd2dd1f493c1a9bc6d5
waner <wanersbh@gmail.com>
2019-10-26 07:16:40 -0300

    Inclusao eureka modulo comunicacao

--- sca-seguranca-comunicao/src/main/java/com/sca/seguranca/comunicaco/ScaSegurancaComunicaoApplication.java
+++ sca-seguranca-comunicao/src/main/java/com/sca/seguranca/comunicaco/ScaSegurancaComunicaoApplication.java
@@ -3,7 +3,9 @@ package com.sca.seguranca.comunicaco;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 @EnableRabbit
 public class ScaSegurancaComunicaoApplication {
